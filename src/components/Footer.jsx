533de5af50fe33d1acfc072a914d766b06863fbd
erhabot <erhabot@gmail.com>
2024-05-12 07:37:56 +0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div id="copyright">
+      <div className="wrapper">
+        &copy; 2024. <b>Ridwanharahap.com</b> All Rights Reserved.
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
